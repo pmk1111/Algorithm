790ca5fbe2dd70a812677018fde4e7118be7cbe4
pmk1111 <kingpmk104@naver.com>
2023-12-28 22:30:17 +0900

    12-28

---
+++ algorithm_study/src/스택/_17298.java
@@ -0,0 +1,48 @@
+package 스택;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class _17298 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                result[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            result[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            bw.write(result[i] + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
